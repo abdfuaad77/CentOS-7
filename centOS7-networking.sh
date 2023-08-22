d69a37d0aa7126d8660272edb530c22f72843e8a
abdfuaad77 <105165367+abdfuaad77@users.noreply.github.com>
2023-08-22 09:38:12 -0500

    Create centOS7-networking.sh
    Replace default NetworkingManager.service with network.service and assign additional subnets

---
+++ centOS7-networking.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+################################################################
+# Interactive Kitty Script
+#
+# OS: CentOS 7.x
+# Replace default NetworkingManager.service with network.service
+# Assign additional subnets
+#
+###############################################################
+read -p "What is your active NIC?: " nic
+&&
+systemctl stop NetworkManager.service
+&&
+systemctl disable NetworkManager.service
+&&
+cd /etc/sysconfig/network-scripts
+&&
+echo "NM_CONTROLLED=no" >> ifcfg-$nic
+&&
+systemctl enable network.service
+&&
+chkconfig network on
+&&
+yum remove NetworkManager -y
+&&
+service network restart
+&&
+echo "Yay, it works, did you want to proceed with assigning the /24 subnet?: "
+select yn in "Yes" "No"; do
+        case $yn in
+               Yes )  echo "Lovely, please fill the required /24 information."
+                         touch ifcfg-$nic-range0
+                         read -p "IP START: " ipstart && read -p "IP END: " ipend && read -p "PREFIX: " prefix && read -p "COLUM START: " column
+echo "IPADDR_START=$ipstart
+IPADDR_END=$ipend
+PREFIX=$prefix
+CLONENUM_START=$column
+ONBOOT=yes
+NM_CONTROLLED=no
+ARPCHECK=no" > testing-purposes.txt
+echo "That lovely information has been saved inside it's respected conf file. Don't forget to restart your network service ;)"
+                                  break;;
+               No )    echo "
+
+That would be considered anticlimactic in some parts of the world.
+
+
+
+QQ SAD FACE"
+                       exit 1;;
+        esac
+    done
